c5ade3040afd28c83a23355707c0e6d4834f434f
Ronaldo Barnes <krazydragon@fullsail.com>
2013-01-09 12:30:33 -0800

    Created Linear Layout and textview

--- src/com/java1/ronaldo/barnes/week1/MainActivity.java
+++ src/com/java1/ronaldo/barnes/week1/MainActivity.java
@@ -3,13 +3,30 @@ package com.java1.ronaldo.barnes.week1;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	LinearLayout llayout;
+	LinearLayout.LayoutParams lparams;
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        
+        llayout = new LinearLayout(this);
+        llayout.setOrientation(LinearLayout.VERTICAL);
+        lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        llayout.setLayoutParams(lparams);
+        
+        TextView tview = new TextView(this);
+        tview.setText("Test");
+        
+        llayout.addView(tview);
+        setContentView(llayout);
+        
     }
 
     @Override
